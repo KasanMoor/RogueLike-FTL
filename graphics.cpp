2fc21411596ecce9eb9a91bde711294f6bbc00d1
KasanMoor <brandonb.junk@gmail.com>
2015-11-24 18:22:58 -0800

    Update graphics.cpp

--- graphics.cpp
+++ graphics.cpp
@@ -1,48 +1,120 @@
-/* cursed adventure v2
- * Brandon Brewster
- */
+//			Brandon Brewster
+//			3/30/15
+//			lab 9
 
-#include <iostream>
-#include <fstream>
-#include <string>
+
+
+#include <iostream> 
 #include <curses.h>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 #include "graphics.h"
 
-/* necessary classes
- *
- * #include "character.h"
- * #include "item.h"
- * #include "npc.h"
- */
-
-int main()
+graphics::graphics()
 {
-    int input;
-    string mapName = "ship.map";
-    graphics window;
-    keypad( stdscr, TRUE );
-    window.setup( mapName );
+    numcols = 70;
+    numrows = 34;
+    defaultx = 0;
+    defaulty = numrows;
+    player.x = 50;
+    player.y = 15;
+    player.id = '@';
+    
+    wnd = initscr();	//initializes the window
+    noecho();
+    clear();
+    refresh();
+}
 
-    input = getch();
-    bool done = false;
-    while( !done )
+graphics::~graphics()
+{
+    endwin();
+    cout << "The window has been returned." << endl;
+}
+
+/* Sets up the room, adding walls, and the robot */
+void graphics::setup( string map ) 
+{
+    ifstream data;
+    data.open( "ship.map" );
+    
+    int i;
+	    
+    for( int i = 0; i < numrows; i++ )
     {
-	input = getch();
-	switch( input ) 
+    char c = '0';
+	for( int j = 0; c != ';'; j++ )
 	{
-	    case KEY_UP: window.moveActor( 'N' );
-		break;
-	    case KEY_DOWN: window.moveActor( 'S' );
-		break;
-	    case KEY_LEFT: window.moveActor( 'W' );
-		break;
-	    case KEY_RIGHT: window.moveActor( 'E' );
-		break;
-	    default: done = true;
+	    data >> noskipws >> c;
+	    if( c != ';' ) {
+		move ( i, j );
+		delch();
+		insch( c );
+	    }
 	}
     }
-    return 0;
+
+    move( player.y, player.x );
+    delch();
+    insch( player.id );
+
+    refresh();
 }
+
+char graphics::bumpCheck(char direction )
+{
+    int checkRow = player.y;
+    int checkColumn = player.x;
+    switch (direction) {
+    case 'W':
+ 	checkColumn = player.x-1;
+	break;
+    case 'E':
+	checkColumn = player.x+1;
+	break;
+    case 'N':
+	checkRow = player.y-1;
+	break;
+    case 'S':
+	checkRow = player.y+1;
+	break;
+    default: return true;
+    }
+
+    move(checkRow, checkColumn);
+    return inch();
+}
+
+void graphics::moveActor( char direction )
+{
+    char bump;
+    if( bumpCheck( direction ) == ' ' ) {
+	move(player.y, player.x);
+	delch();
+	insch(' ');
+	switch (direction) {
+	case 'W': 
+	    player.x -= 1;
+	    break;
+	case 'E':
+	    player.x += 1;
+	    break;
+	case 'N':
+	    player.y -= 1;
+	    break;
+	case 'S':
+	    player.y += 1;
+	    break;
+	}
+	move(player.y, player.x);
+	delch();
+	insch(player.id);
+
+    }
+    move( defaulty, defaultx );
+    refresh();
+}
+
